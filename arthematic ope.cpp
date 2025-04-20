0d49c482adaa762379c87777f2f93e3af1681cdf
Duaheesbani <duaheesbani@gmail.com>
2025-04-20 20:52:39 +0500

    Add files via upload

---
+++ arthematic ope.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	cout<<"enter your first variable";
+	cin>>a;
+	cout<<"enter your second variable";
+	cin>>b;
+	
+	cout<<"a+b is "<<a+b<<endl;
+	cout<<"a-b is "<<a+b<<endl;
+	cout<<"a/b is "<<a+b<<endl;
+	cout<<"a*b is "<<a+b<<endl;
+	
+	return 0;
+	
+}
